764d6d335ea0728c415321071646735f695ba7d3
Eamonn de Leastar <edeleastar@gmail.com>
2014-11-06 18:31:04 +0000

    rename user table

--- app/models/User.java
+++ app/models/User.java
@@ -3,11 +3,14 @@ package models;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import java.util.List;
 
 import play.db.jpa.Model;
 
 @Entity
+@Table(name="`User`") //This is necessary because User is a reserved word in PostGreSQL
 public class User extends Model
 {
   public String firstName;
